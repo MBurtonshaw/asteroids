2f8dd759de532a51ba98762ccc52eab6fcaed7f7
Matt <mburtonshaw@gmail.com>
2023-12-28 16:29:42 -0500

    initial setup

---
+++ index.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+c.fillStyle = 'black';
+c.fillRect(0, 0, canvas.width, canvas.height);
