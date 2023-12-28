8b75966a6f745a8045520f1bf6b9ec0e42b0fbc2
Matt <mburtonshaw@gmail.com>
2023-12-28 17:18:28 -0500

    players creation

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 c.fillStyle = 'black';
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Player {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.moveTo(this.position.x + 30, this.position.y);
+        c.lineTo(this.position.x - 10, this.position.y - 10);
+        c.lineTo(this.position.x - 10, this.position.y + 10);
+        c.closePath();
+
+        c.strokeStyle = 'white';
+        c.stroke();
+    }
+}
+
+const player = new Player({ 
+    position: {x: canvas.width/2, y: canvas.height/2},
+    velocity: {x:0, y:0}
+ });
+
+ player.draw();
+ 
